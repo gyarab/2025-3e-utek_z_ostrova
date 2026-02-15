2440967a8ece6e4c8d3f77ed9e0cb3337f52bd4a
sicelet1 <jakub.wimmer7@gmail.com>
2026-02-15 16:04:19 +0100

    added basic error handle functions

--- Game/Game/error_handle.cpp
+++ Game/Game/error_handle.cpp
@@ -3,8 +3,32 @@
 namespace ErrorHandle //[start]
 {
 
-//Add game-wide Error handle!
-//Handle errors using popup windows!
+//Add game-wide Error handle! <- make better soon!
+
+//Place an error related to specified code and terminate process eventually
+void Report(const std::string& _ErrorMessage, const uint64_t _ErrorCode, const bool _TerminateProcess)
+{
+	RuntimeLog::Message(ERROR, _ErrorMessage);
+
+	if (!_TerminateProcess)
+		return;
+
+	RuntimeLog::Message(CRASH, "previous error was too fatal => process terminated");
+	SDL_QuitSubSystem(SDL_INIT_VIDEO);
+	SDL_Quit();
+	//Process ends here - function doesnt return
+	std::exit(_ErrorCode);
+
+	return;
+};
+
+//Place an error related to SDL-lib and terminate process eventually
+void ReportSDL(const bool _TerminateProcess)
+{
+	ErrorHandle::Report(SDL_GetError(), -1, _TerminateProcess);
+
+	return;
+};
 
 }
 //ErrorHandle [end]
---
+++ Game/Game/error_messages_codes.hpp
@@ -0,0 +1,7 @@
+#ifndef ERROR_MESSAGES_CODES
+#define ERROR_MESSAGES_CODES
+
+//All possible errors this program can produce
+
+
+#endif
