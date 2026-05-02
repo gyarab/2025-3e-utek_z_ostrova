0719d680231cec0f293be2998f1e107730ef488c
sicelet1 <jakub.wimmer7@gmail.com>
2026-05-02 15:23:04 +0200

    added concept of having a struct that will hold all the data from the config file which will make it to be loaded only once when the app starts

--- Game/Game/config_file_content_info.hpp
+++ Game/Game/config_file_content_info.hpp
@@ -12,7 +12,7 @@
 #define SELECTOR_tRatio "tRatio"
 #define SELECTOR_TextureScalingCoefficient "TextureScalingCoefficient"
 #define SELECTOR_tWindowTitle "tWindowTitle"
-#define SELECTOR_fpsCap "fpsCap"
+#define SELECTOR_fpsCap "FpsCap"
 #define SELECTOR_tAppDir "tAppDir"
 #define SELECTOR_bRuntimeLogEnabled "bRuntimeLogEnabled"
 #define SELECTOR_tLogDir "tLogDir"
@@ -25,4 +25,37 @@
 #define SELECTOR_tLevel1Textures "tLevel1Textures"
 #define SELECTOR_Level1TexturesWidth "Level1TexturesWidth"
 #define SELECTOR_Level1TexturesLength "Level1TexturesLength"
+
+/*
+typedef struct AllActuallConfigFileContentValues
+{
+	//[AppWindowConfig]
+	int64_t _Width;
+	int64_t _Length;
+	std::string _Ratio; //maybe remove!
+	int64_t _TextureScalingCoefficient;
+	std::string _WindowTitle;
+	int64_t _FpsCap;
+
+	//[AppConfig]
+	std::string _AppDir;
+	bool _RuntimeLogEnabled;
+	std::string _LogDir;
+	int64_t _MaxLogFiles;
+
+	//[AppAssetConfig]
+	std::string _GraphicsDir;
+	std::string _TextureFormat;
+	std::string _PlayerTextures;
+	int64_t _PlayerTexturesWidth; //maybe remove!
+	int64_t _PlayerTexturesLength; //maybe remove!
+	std::string _Level1Textures;
+	int64_t _Level1TexturesWidth; //maybe remove!
+	int64_t _Level1TexturesLength; //maybe remove!
+}
+Config;
+
+static Config ConfigValues;
+*/
+
 #endif
