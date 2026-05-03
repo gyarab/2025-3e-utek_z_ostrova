79a98ef44a88efb881bbd9642fae5a4a58ccbd20
sicelet1 <jakub.wimmer7@gmail.com>
2026-05-03 15:14:24 +0200

    added function that allow both animating its textures and moving its hitbox on the same thread

--- Game/Game/entity.hpp
+++ Game/Game/entity.hpp
@@ -14,20 +14,25 @@ enum EntityVectorIndexes
 //Container defining a state of an entity
 struct entity
 {
-	//Two numbers [_Vector[0] = dx ; _Vector[1] = dy] defining the direction which the entity will be facing / moving into
+	//Two numbers [_Vector[0] = dx ; _Vector[1] = dy] defining the direction which the entity will be facing / moving into usually related to movespeed and jump power
 	int64_t _Vector[2];
 	//Bool that tells if the entity is in the moving state
 	bool _IsMoving;
+	//Bool that tells whether the entity is alive [hp > 0] or not
+	bool _IsAlive;
+	
 	//Rectange defining both position and size of the entity [not its texture directly]
 	SDL_FRect _Hitbox;
+	//Bool that tells whether the entity is immortal or not
+	bool _IsImmortal;
 	//A non-negative number that tells how many hp does the entity have [0 = dead]
 	int64_t _Health;
 	//A non-negative number that tells how many hp can the entity take away from another entity [0 = no damage]
 	int64_t _Damage;
-	//Bool that tells whether the entity is alive [hp > 0] or not
-	bool _IsAlive;
-	//Bool that tells whether the entity is immortal or not
-	bool _IsImmortal;
+	//Number that tells how much will the entity move
+	uint64_t _Movespeed;
+	//Number that tells how high the entity will jump
+	uint64_t _JumpPower;
 
 	//All posible textures the entity can use to display itself
 	TCluster _Textures;
@@ -37,6 +42,8 @@ public:
 	INLINE void load_basic_info(const std::string& _SpecificDB_Filename);
 	//Function that changes _Hitbox position based on the _Vector values once only if _IsMoving is true and its alive or immortal
 	INLINE void make_one_movement(void);
+	//Function that animates the entitys TCluster and changes _Hitbox position based on the _Vector values once only if _IsMoving is true and its alive or immortal
+	INLINE void make_movement_while_animating(const std::chrono::milliseconds _TextureUpdateDelay, std::mutex* _OptionalThreadMutex = nullptr);
 	//Function that takes away health of another entitys hp [if its not immortal and its alive] based on this entitys dmg - cant go to negative
 	INLINE void deal_damage_to(entity& _EntityToBeDamaged);
 	//Function that determines whether on entitys hitbox is inside the onther entitys hitbox or vise versa
